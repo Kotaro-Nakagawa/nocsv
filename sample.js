50d1087f252ad4d4209c12ceab9d87e502ada40d
Kotaro Nakagawa <kootaroonakagawa@gmail.com>
2023-09-10 22:37:12 +0900

    テーブルの幅をオプションで指定できるように修正

--- sample.js
+++ sample.js
@@ -6,15 +6,22 @@ const editorElement = document.getElementById('editor')
 const editor = new Editor(editorElement)
 const inputElement = document.getElementById('input')
 const outputElement = document.getElementById('output')
-const importButton = document.getElementById('testbutton')
 
+const importButton = document.getElementById('testbutton')
 importButton.onclick = () => {
   editor.import(JSON.parse(inputElement.value))
   outputElement.textContent = JSON.stringify(editor.export(), undefined, 2);
 }
 
 const exportButton = document.getElementById('exportbutton')
-
 exportButton.onclick = () => {
   outputElement.textContent = JSON.stringify(editor.export(), undefined, 2);
+}
+
+/**
+ * @type {HTMLInputElement}
+ */
+const tableWidthSlider = document.getElementById('tableWidth')
+tableWidthSlider.oninput = () => {
+  editor.displayConfig.setMainWidth(parseInt(tableWidthSlider.value, 10))
 }
